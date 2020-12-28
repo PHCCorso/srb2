7b41c403589b8dafa30df33b7b44b33d410a47f3
Pedro Corso <pedro.corso@fleetster.net>
2020-12-28 06:46:54 +0100

    Releasing my little mods

---
+++ Thanos Mode/VL_rewind.lua
@@ -0,0 +1,260 @@
+/* 
+	Author: PHCC
+	Date: 28/12/2020
+
+    TIME POWAS!
+    
+    To be futurely used as the power for the green emerald (time stone)
+
+    Press BT_CUSTOM2 to rewind!
+    
+*/
+
+local MAX_REWIND_CHAIN_LENGTH = 10 * TICRATE
+local TIMESTONE_DRAIN = 1
+
+function A_GetPlayerState(player) // Imma making every function global because local scope sucks
+	local state = {}
+	state.powers = {}
+
+	// Only the relevant powers, we dont want to bring back shields/invincibility/etc
+	state.powers[pw_flashing] = player.powers[pw_flashing]
+	state.powers[pw_carry] = player.powers[pw_carry]
+	state.powers[pw_tailsfly] = player.powers[pw_tailsfly]
+	state.powers[pw_spacetime] = player.powers[pw_spacetime]
+	state.powers[pw_underwater] = player.powers[pw_underwater]
+	state.powers[pw_nocontrol] = 1<<15
+
+	if (player.followmobj)
+		state.followmobjstate = A_GetMobjState(player.followmobj) // For Tails and Metal Sonic
+	end
+
+	state.pflags = player.pflags | PF_FULLSTASIS // stop moving
+
+	// Interesting data to keep track of, not sure if this is necessary, though
+	state.jumping = player.jumpingw
+	state.secondjump = player.secondjump
+	state.fly1 = player.fly1
+	state.glidetime = player.glidetime
+	state.climbing = player.climbing
+
+	return state
+end
+
+function A_SetPlayerState(player, state)
+	player.powers[pw_flashing] = state.powers[pw_flashing]
+	player.powers[pw_carry] = state.powers[pw_carry]
+	player.powers[pw_tailsfly] = state.powers[pw_tailsfly]
+	player.powers[pw_spacetime] = state.powers[pw_spacetime]
+	player.powers[pw_underwater] = state.powers[pw_underwater]
+
+	if (state.followmobjstatew)
+		A_SetMobjState(player.followmobj, state.followmobjstate)
+	end
+
+	player.pflags = state.pflags
+	player.jumping = state.jumping
+	player.secondjump = state.secondjump
+	player.fly1 = state.fly1
+	player.glidetime = state.glidetime
+	player.climbing = state.climbing
+end
+
+function A_GetMobjState(mo, stack)
+	local state = {}
+	if (not(stack)) // Yup, a stack. You will understand it later
+		stack = {}
+	end
+	stack[mo] = state
+
+	if (mo.player)
+		state.player = A_GetPlayerState(mo.player)
+	end
+
+	// Alright, imma copy everything I can
+	state.x = mo.x
+	state.y = mo.y
+	state.z = mo.z
+	state.momx = mo.momx
+	state.momy = mo.momy
+	state.momz = mo.momz
+	state.angle = mo.angle
+	state.rollangle = mo.rollangle
+	state.state = mo.state
+	state.frame = mo.frame
+	state.health = mo.health
+	state.flags = mo.flags
+	if (mo.type == MT_PLAYER)
+		state.flags = $ | MF_NOCLIP // Lets make the player able to rewind through walls
+	end
+	state.flags2 = mo.flags2
+	state.eflags = mo.eflags
+	state.spawnpoint = mo.spawnpoint
+	state.tics = mo.tics
+	state.scale = mo.scale
+	state.fuse = mo.fuse
+	state.extravalue1 = mo.extravalue1
+	state.extravalue2 = mo.extravalue2
+	if (mo.tracer and mo.tracer.type ~= MT_PLAYER) // The tracer comes with us
+		state.tracer = mo.tracer
+		state.tracertype = mo.tracer.type
+		if (stack[mo.tracer])
+			state.tracerstate = stack[mo.tracer] // This means we have already stored the state of that mobj. Avoid infinite recursion.
+		else
+			state.tracerstate = A_GetMobjState(state.tracer, stack) // Why stop at the top level?
+		end
+	end
+	if (mo.target) // Same thing for the target
+		state.target = mo.target
+		state.targettype = mo.target.type
+		if (stack[mo.target])
+			state.targetstate = stack[mo.target]
+		else
+			state.targetstate = A_GetMobjState(state.target, stack)
+		end
+	end
+
+	return state
+end
+
+function A_SetMobjState(mo, state, stack)
+	if (not(stack))
+		stack = {} // Stack here again
+	end
+	stack[mo] = state
+
+	if (mo.player)
+		A_SetPlayerState(mo.player, state.player)
+	end
+
+	P_TeleportMove(mo, state.x, state.y, state.z)
+	mo.momx = state.momx
+	mo.momy = state.momy
+	mo.momz = state.momz
+	mo.angle = state.angle
+	mo.rollangle = state.rollangle
+	mo.state = state.state
+	mo.frame = state.frame
+	mo.health = state.health
+	mo.flags = state.flags
+	mo.flags2 = state.flags2
+	mo.eflags = state.eflags
+	mo.spawnpoint = state.spawnpoint
+	mo.tics = state.tics
+	mo.scale = state.scale
+	mo.fuse = state.fuse
+	mo.extravalue1 = state.extravalue1
+	mo.extravalue2 = state.extravalue2
+
+	if (state.tracer and not(stack[state.tracer]))
+		if (state.tracer.valid)
+			A_SetMobjState(state.tracer, state.tracerstate, stack)
+		elseif (mo.tracer and mo.tracer.valid) // If we spawned a new tracer from a previous state, then we get it from here
+			A_SetMobjState(mo.tracer, state.tracerstate, stack)
+		else // If the tracer is gone, spawn it back! Specially relevant for minecarts
+			local newtracer = P_SpawnMobj(state.tracerstate.x, state.tracerstate.y, state.tracerstate.z, state.tracertype)
+			A_SetMobjState(newtracer, state.tracerstate, stack)
+			mo.tracer = newtracer
+		end
+	else
+		mo.tracer = state.tracer // Probably nil, but leave it here
+	end
+	if (state.target and not(stack[state.target])) // Exact same thing done with the tracer, too lazy to make it generic
+		if (state.target.valid)
+			A_SetMobjState(state.target, state.targetstate, stack)
+		elseif (mo.target and mo.target.valid)
+			A_SetMobjState(mo.target, state.targetstate, stack)
+		else
+			local newtarget = P_SpawnMobj(state.targetstate.x, state.targetstate.y, state.targetstate.z, state.targettype)
+			A_SetMobjState(newtarget, state.targetstate, stack)
+			mo.target = newtarget
+		end
+	else
+		mo.target = state.target
+	end
+end
+
+
+
+local function addNewStateToPlayerChain(player, state) // If you dont understand linked lists, then what are you doing here?
+	if (not(player.rewindchain))
+		player.rewindchain = {}
+	end
+
+	local chain = player.rewindchain
+
+	if (not(chain.laststate))
+		state.prevstate = chain
+		chain.nextstate = state
+		chain.laststate = state
+	else
+		state.prevstate = chain.laststate
+		chain.laststate.nextstate = state
+		chain.laststate = state
+	end
+
+	if(not(chain.length))
+		chain.length = 1
+	else
+		chain.length = $ + 1
+	end	
+
+	if (chain.length == MAX_REWIND_CHAIN_LENGTH) // Limit was reached, we do it FIFO and throw away the oldest state
+		player.rewindchain = chain.nextstate
+		player.rewindchain.length = chain.length - 1
+		chain.nextstate.laststate = chain.laststate
+		chain.nextstate.prevstate = nil // Clean references just to be sure
+	end
+end
+
+local function rewind(player) // Where the magic happens
+	if (player.rings - TIMESTONE_DRAIN <= 0) return end // No rings, no back in time. But we leave you a ring so you dont die
+	
+	if (player.rewindchain.length > 1 and player.rewindchain.length % TICRATE == 0) // We only drain rings every second
+		player.rings = $ - TIMESTONE_DRAIN
+	end
+
+	local chainnode = player.rewindchain.laststate
+	
+	if (not(chainnode) or not(chainnode.prevstate)) return end
+
+	A_SetMobjState(player.mo, chainnode) // But the real magic is here
+
+	player.rewindchain.length = $ - 1
+	player.rewindchain.laststate = chainnode.prevstate
+	chainnode.prevstate.nextstate = nil // Clean references just to be sure 2
+	chainnode.prevstate = nil // Clean references just to be sure 3
+end
+
+addHook("MobjDeath", function(target)
+	local tracer = {} // For debugging
+	if (target.tracer)
+		tracer = target.tracer
+	end
+	print(string.format("target: %s, tracer: %s", tostring(target.type), tostring(tracer.type)))
+
+	if (target.player and target.player.rewinding) // No dying while rewinding. Specially useful for minecarts.
+		target.health = 1
+		return true
+	end
+end)
+
+addHook("ThinkFrame", function() // The intention is for this to work with all skins
+	for player in players.iterate
+		if (player.cmd.buttons & BT_CUSTOM2)
+			player.rewinding = true
+			rewind(player)
+		else
+			player.rewinding = false
+			player.mo.flags = $ & ~MF_NOCLIP
+			player.powers[pw_nocontrol] = 0
+			addNewStateToPlayerChain(player, A_GetMobjState(player.mo))
+		end
+	end
+end)
+
+addHook("MapChange", function(gamemap)
+	for player in players.iterate
+		player.rewindchain = {} // Clean everything when map changes
+	end
+end)
---
+++ Thanos Mode/VL_thanosmode.lua
@@ -0,0 +1,163 @@
+/* 
+	Author: PHCC
+	Date: 28/12/2020
+
+    Rules:
+      - You should kill half of the enemies of the stage in order to proceeed
+      - Once every chaos emerald is collected, you will be able to "snap your fingers" by pressing BT_CUSTOM1, but at a cost:
+        - 200 rings or
+        - For each missing 100 rings, you give a life in exchange (and you lose your rings, of course) 
+*/
+
+local canexit = false
+local enemytable = {}
+local enemyindex = 1
+local enemiesonload = 0
+local enemieskilled = 0 // This and the next one are used to handle levels with an increasing amount of enemies (e.g. Aerial Garden Zone)
+local newenemieskilled = 0
+
+local function isEnemy(mo)
+	if ((mo.flags & MF_ENEMY and not(mo.flags & MF_SOLID)) or mo.flags & MF_BOSS) // A power so great that it can kill bosses... at a high cost
+		return true
+	end
+	return false
+end
+
+local function setExitSectorEnabled(enabled) // Hacky stuff to disable the exit sector
+	for sec in sectors.iterate do
+		if (GetSecSpecial(sec.special,4) == 2 or GetSecSpecial(sec.special,4) == 10)
+			if (enabled)
+				sec.special = 2<<12
+			else
+				sec.special = 10<<12 // I hope no co-op levels use this
+			end
+		end
+	end
+
+	canexit = enabled // Used to avoid message spamming
+end
+
+local function enemiesLeft() // Dont question my math!
+	local extraenemies = #enemytable - enemiesonload - newenemieskilled
+
+	local left = (enemiesonload + extraenemies)/2 - enemieskilled
+
+	if (left < 0)
+		return 0
+	end
+
+	return left
+end
+
+local function snapFingers(player) // BEHOLD THE POWER OF THE CHAOS EMERALDS!!!
+	if (not(All7Emeralds(emeralds)) and not(player.fingerssnapped)) return end
+
+	if (player.rings < 200) // You either feed the power of chaos with enough rings or you pay for it with YOUR LIFE!
+		local livestoremove = 2 - player.rings/100
+
+		if (player.lives - livestoremove < 1) return end
+
+		player.rings = 0
+		player.lives = $ - livestoremove // How much did it cost?
+	else
+		player.rings = $ - 200 
+	end
+
+	P_BlackOw(player) // Why not give it some extra blast?
+
+	while enemiesLeft()
+		for _,enemy in ipairs(enemytable)
+			if (enemy and P_RandomChance(FRACUNIT/2))
+				P_KillMobj(enemy, player.mo)
+			end
+
+			if (not(enemiesLeft()))
+				break
+			end
+		end
+	end 
+
+	player.fingerssnapped = true // only once per player
+end
+
+addHook("MobjSpawn", function(mo)
+	if (not(gametype == GT_COOP) or G_IsSpecialStage()) return end
+
+	if (isEnemy(mo))
+		enemytable[enemyindex] = mo
+		mo.enemyindex = enemyindex
+
+		if (leveltime > 0)
+			mo.newenemy = true
+		end
+
+		enemyindex = $ + 1
+	end
+end)
+
+addHook("MobjDeath", function(target)
+	if (not(gametype == GT_COOP) or G_IsSpecialStage()) return end
+	if (not(isEnemy(target))) return end
+
+	enemytable[target.enemyindex] = false
+
+	if (target.newenemy)
+		newenemieskilled = $ + 1
+	else
+		enemieskilled = $ + 1
+	end
+		
+	if (enemiesLeft() == 0 and not(canexit))
+		setExitSectorEnabled(true)
+		print("You helped to bring balance to the universe and may now finish the level")
+	elseif (enemiesLeft() > 0 and canexit) // Some new enemies popped up
+		print("There are more enemies around, kill them first!")
+		setExitSectorEnabled(false)
+	end
+end)
+
+addHook("MapChange", function(gamemap) // Resetting
+	canexit = false
+	enemytable = {}
+	enemyindex = 1
+	enemiesonload = 0
+	enemieskilled = 0
+	newenemieskilled = 0
+end)
+
+addHook("MapLoad", function()
+	enemiesonload = #enemytable
+
+	if (enemiesonload % 2 == 1) 
+		enemieskilled = -1 // because yes
+	end
+
+	for player in players.iterate
+		player.fingerssnapped = false
+	end
+
+	setExitSectorEnabled(false)
+end)
+
+addHook("ThinkFrame", function()
+	for player in players.iterate
+		if (not(player.prevstate))
+			player.prevstate = {}
+		end
+
+		if (player.cmd.buttons & BT_CUSTOM1 and not(player.prevstate.buttons & BT_CUSTOM1))
+			snapFingers(player)
+		end
+
+		player.prevstate.buttons = player.cmd.buttons
+	end
+end)
+
+local function drawcounter(drawfunc, p, c)
+	if (not(gametype == GT_COOP) or G_IsSpecialStage()) return end
+
+	drawfunc.drawString(258, 190, enemiesLeft(), V_SNAPTOBOTTOM|V_SNAPTORIGHT)
+	drawfunc.drawString(288, 190, "left", V_SNAPTOBOTTOM|V_SNAPTORIGHT)
+end
+
+hud.add(drawcounter)
