f1e16c2c0187940ef582a6edb0c2605abd03db22
Pedro Corso <pedro.corso@fleetster.net>
2021-01-02 15:17:20 +0100

    More improvements on our mind projection power, still needs fixing

--- Thanos Mode/VL_mind.lua
+++ Thanos Mode/VL_mind.lua
@@ -17,20 +17,21 @@
 
 local MINDSTONE_DRAIN = 0
 local PROJECTION_DURATION = 35*TICRATE
+local GASPSTATETICS = TICRATE/2
 
-local function doAstralProjection(player) // Disembody yourself
+local function doAstralProjection(player) // Disembody yourself (inspired on P_SpawnGhostMobj)
   local mo = player.mo
   local body = P_SpawnMobj(mo.x, mo.y, mo.z, MT_PLAYER)
 
   // mobj info
   body.angle = mo.angle
+  body.scale = mo.scale
   body.sprite = mo.sprite
   body.sprite2 = mo.sprite2
   body.state = S_PLAY_STND
   body.flags = mo.flags & ~MF_SOLID
   body.flags2 = mo.flags2
   body.eflags = mo.eflags
-  body.tics = -1
   body.skin = mo.skin
   body.color = mo.color
   body.colorized = mo.colorized
@@ -39,7 +40,32 @@ local function doAstralProjection(player) // Disembody yourself
   body.powers = {}
   body.powers[pw_spacetime] = player.powers[pw_spacetime]
   body.powers[pw_underwater] = player.powers[pw_underwater]
+  body.powers[pw_shield] = player.powers[pw_shield]
   body.charflags = player.charflags
+
+  // random thing
+  body.gaspstatetics = 0
+
+  if (player.followmobj)
+    local followmobj = P_SpawnMobj(player.followmobj.x, player.followmobj.y, player.followmobj.z, player.followitem)
+    followmobj.angle = player.followmobj.angle
+    followmobj.sprite = player.followmobj.sprite
+    followmobj.sprite2 = player.followmobj.sprite2
+    followmobj.state = player.followmobj.state
+    followmobj.flags = player.followmobj.flags & ~MF_SOLID
+    followmobj.eflags = player.followmobj.eflags
+    followmobj.skin = player.followmobj.skin
+    followmobj.color = player.followmobj.color
+    followmobj.colorized = player.followmobj.colorized
+
+    followmobj.flags2 = player.followmobj.flags2 & ~MF2_LINKDRAW // Solving dispoffset issues
+
+    followmobj.target = body
+    followmobj.target = body
+    body.tracer = followmobj
+
+    body.followmobj = followmobj
+  end
 
   // Connect our ghost to our body, and our body to our ghost
   body.projection = mo
@@ -48,12 +74,13 @@ local function doAstralProjection(player) // Disembody yourself
   P_Thrust(mo, mo.angle, 20*FRACUNIT)
 end
 
-local function endAstralProjection(body) // We are bringing our ghost/soul back to our body
+local function endAstralProjection(body, damaged) // We are bringing our ghost/soul back to our body
   local player = body.projection.player
   local mo = player.mo
 
   P_TeleportMove(mo, body.x, body.y, body.z)
   mo.angle = body.angle
+  mo.scale = body.scale
   mo.sprite = body.sprite
   mo.sprite2 = body.sprite2
   mo.state = body.state
@@ -63,11 +90,22 @@ local function endAstralProjection(body) // We are bringing our ghost/soul back 
   mo.eflags = body.eflags
   player.powers[pw_spacetime] = body.powers[pw_spacetime]
   player.powers[pw_underwater] = body.powers[pw_underwater]
+
+  if(not(damaged)) // To handle special cases
+    player.powers[pw_shield] = body.powers[pw_shield] // Give back the shield
+  end
   P_SpawnShieldOrb(player)
 
   player.pflags = $ & ~PF_NOCLIP
 
   player.body = nil
+
+  if (body.followmobj)
+    P_RemoveMobj(body.followmobj)
+  end
+  if (body.shield)
+    P_RemoveMobj(body.shield)
+  end
   P_RemoveMobj(body)
 end
 
@@ -105,28 +143,43 @@ local function spawnBodyBubbles(body) // Shamelessly stolen from srb2 sourcecode
 end
 
 local function handleBodyDrowning(player)
+  local body = player.body
+
   // If body was left underwater or in space, then let the counter run
-  if (player.body.powers[pw_spacetime])
-    player.body.powers[pw_spacetime] = $ - 1
-    if (player.body.powers[pw_spacetime] <= 0) // Time is up, so you die
-      endAstralProjection(player.body)
+  if (body.powers[pw_spacetime])
+    body.powers[pw_spacetime] = $ - 1
+    if (body.powers[pw_spacetime] <= 0) // Time is up, so you die
+      endAstralProjection(body, true)
       P_KillMobj(player.mo, nil, nil, DMG_SPACEDROWN)
+      return
     end
   end
-  if (player.body.powers[pw_underwater])
-    player.body.powers[pw_underwater] = $ - 1
-    if (player.body.powers[pw_underwater] <= 0)
-      endAstralProjection(player.body)
+  if (body.powers[pw_underwater])
+    body.powers[pw_underwater] = $ - 1
+    if (body.powers[pw_underwater] <= 0)
+      endAstralProjection(body, true)
       P_KillMobj(player.mo, nil, nil, DMG_DROWNED)
+      return
     end
   end
 
-  // Setting the timers for our player as well
-  player.powers[pw_spacetime] = player.body.powers[pw_spacetime]
-  player.powers[pw_underwater] = player.body.powers[pw_underwater]
+  // Setting the timers for the player as well
+  player.powers[pw_spacetime] = body.powers[pw_spacetime]
+  player.powers[pw_underwater] = body.powers[pw_underwater]
 
-  // Why not also spawn some bubbles?
-  spawnBodyBubbles(player.body)
+  // Spawn some bubbles to make it look real
+  spawnBodyBubbles(body)
+
+  if (body.gaspstatetics == GASPSTATETICS) // We just breathed an air bubble
+    body.state = S_PLAY_GASP
+    S_StartSound(body, 13)
+    body.powers[pw_underwater] = underwatertics
+    body.gaspstatetics = $ - 1
+  elseif (body.gaspstatetics > 0)
+    body.gaspstatetics = $ - 1
+  else
+    body.state = S_PLAY_STND
+  end
 end
 
 local function handlePlayerProjection(player)
@@ -134,7 +187,7 @@ local function handlePlayerProjection(player)
   // If button pressed
   if (player.cmd.buttons & BT_CUSTOM2 and not(player.prevcmd & BT_CUSTOM2))
     local mo = player.mo
-    if (mo.momx > 0 or mo.momy > 0 or mo.momz > 0) return end // Just do it while stopped
+    if (mo.momx > 0 or mo.momy > 0 or mo.momz > 0) return end // Just do it while standing still
     
     if (not(player.body))
       if (player.rings - MINDSTONE_DRAIN < 0) return end // Not enough rings, stop it
@@ -153,6 +206,11 @@ local function handlePlayerProjection(player)
     player.mo.frame = $ & ~FF_TRANSMASK
     player.mo.frame = $ | TR_TRANS60
     player.pflags = $ | PF_NOCLIP
+
+    if (player.followmobj)
+      player.followmobj.frame = $ & ~FF_TRANSMASK
+      player.followmobj.frame = $ | TR_TRANS60
+    end
 
     searchBlockmap("objects", function(mo, object) 
       if (object.valid)
@@ -195,24 +253,60 @@ addHook("PlayerCanDamage", function(player, object) // Ghost player also cannot 
     end
 end)
 
+local function handleShieldShit(body, shield)
+  body.shield = P_SpawnMobj(body.x, body.y, body.z, MT_THOK)
+  body.shield.state = shield.state
+  body.shield.dispoffset = 2
+  if (shield.tracer and shield.tracer.valid)
+    body.shield.tracer = P_SpawnMobj(body.x, body.y, body.z, shield.tracer.type)
+    body.shield.tracer.target = body.shield
+    body.shield.tracer.state = shield.tracer.state
+  end
+  
+  // Keep those tails inside that shield!!
+  body.shield.dispoffset = 3
+
+  // Once we have the shield, remove it from the player, we add it back later
+  body.projection.player.powers[pw_shield] = SH_NONE 
+end
+
 addHook("MobjThinker", function(mo) 
   if (mo.target and mo.target.valid)
     local target = mo.target
     if (target.player and target.player.valid)
       if (target.player.body and target.player.body.valid)
-        mo.target = target.player.body // Do not target our ghost, target our body!
+        local body = target.player.body
+
+        // Exception for shields
+        if (mo.flags2 & MF2_SHIELD)
+          handleShieldShit(body, mo)
+          return
+        end
+
+        if (P_CheckSight(mo, body))
+          mo.target = body // Do not target our ghost, target our body!
+        else
+          mo.target = nil // If you cannot see our real body, then stop targetting us
+        end
       end
     end
   end
 end)
 
-// Handling damage for our vulnerable body
 addHook("MobjMoveCollide", function(toucher, touched)
   if (toucher.valid and touched.valid)
-    if (toucher.flags & (MF_ENEMY|MF_BOSS|MF_PAIN|MF_MISSILE|MF_FIRE)
-    and touched.projection and touched.projection.valid)
-      P_DamageMobj(touched.projection, toucher) // If our body was hit, then we damage the player connected to it
-      endAstralProjection(touched) // You got hit, go back to your body
+    if (touched.projection and touched.projection.valid)
+      if (toucher.flags & (MF_ENEMY|MF_BOSS|MF_PAIN|MF_MISSILE|MF_FIRE)) // Handling damage for our vulnerable body
+        local player = touched.projection.player
+        player.powers[pw_shield] = touched.powers[pw_shield] // We give back the shield beforehand so it is handled correctly
+        P_DamageMobj(touched.projection, toucher) // If our body was hit, then we damage the player connected to it
+        endAstralProjection(touched) // You got hit, go back to your body
+      end
+
+      if (toucher.type == MT_EXTRALARGEBUBBLE and toucher.z >= touched.z + touched.height/2) // Handling breathing if left on an air bubble patch
+        P_RemoveMobj(toucher)
+        touched.gaspstatetics = GASPSTATETICS // wait a little to return to stand state
+      end
     end
   end
 end)
@@ -223,7 +317,7 @@ addHook("MapChange", function(player, object)
   for player in players.iterate
     if (player.mo and player.mo.valid)
       player.body = nil
-      resetPlayerFlags(player)
+      player.pflags = $ & ~PF_NOCLIP
     end
 	end
 end)
